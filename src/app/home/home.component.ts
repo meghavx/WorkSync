f7652d4b77435b984f69d7a6331feb11a8439b17
Megha V <meghavx@outlook.com>
2025-02-18 11:26:28 +0530

    move 'header' component out of 'core' folder to 'app' folder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../core/header/header.component';
+import { HeaderComponent } from '../header/header.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
